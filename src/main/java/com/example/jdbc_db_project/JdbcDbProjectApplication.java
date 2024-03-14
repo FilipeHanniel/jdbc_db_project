6c7692d19f35a72dc58d4c9285e7379c197d94a4
Filipe Hanniel <filipehanniel@gmail.com>
2024-03-14 18:57:58 -0300

    Commit inicial - Projeto de aprendizado jdbc

---
+++ src/main/java/com/example/jdbc_db_project/JdbcDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_db_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcDbProjectApplication.class, args);
+	}
+
+}
